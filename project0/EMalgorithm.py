8abde3d21483b94eda249d3115b52cb7e5591297
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2024-12-22 08:43:27 -0600

    Update EMalgorithm.py

--- project0/EMalgorithm.py
+++ project0/EMalgorithm.py
@@ -322,3 +322,125 @@ def fill_matrix(X: np.ndarray, mixture: GaussianMixture) -> np.ndarray:
     X_pred[miss_indices] = (post @ mu)[miss_indices]
     
     return X_pred
+
+from typing import Tuple
+import numpy as np
+from scipy.special import logsumexp
+
+
+def log_gaussian(x: np.ndarray, mean: np.ndarray, var: float) -> float:
+    """Computes the log probablity of vector x under a normal distribution
+
+    Args:
+        x: (d, ) array holding the vector's coordinates
+        mean: (d, ) mean of the gaussian
+        var: variance of the gaussian
+
+    Returns:
+        float: the log probability
+    """
+    d = len(x)
+    log_prob = -d / 2.0 * np.log(2 * np.pi * var)
+    log_prob -= 0.5 * ((x - mean) ** 2).sum() / var
+    return log_prob
+
+def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
+    """E-step: Softly assigns each datapoint to a gaussian component
+
+    Args:
+        X: (n, d) array holding the data, with incomplete entries (set to 0)
+        mixture: the current gaussian mixture
+
+    Returns:
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the assignment
+
+    """
+    # Instructor's solution:
+    n, _ = X.shape
+    K, _ = mixture.mu.shape
+    post = np.zeros((n, K))
+
+    ll = 0
+    for i in range(n):
+        mask = (X[i, :] != 0)
+        for j in range(K):
+            log_likelihood = log_gaussian(X[i, mask], mixture.mu[j, mask],
+                                          mixture.var[j])
+            post[i, j] = np.log(mixture.p[j] + 1e-16) + log_likelihood
+        total = logsumexp(post[i, :])
+        post[i, :] = post[i, :] - total
+        ll += total
+
+    return np.exp(post), ll
+
+def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
+    """M-step: Updates the gaussian mixture by maximizing the log-likelihood
+    of the weighted dataset
+
+    Args:
+        X: (n, d) array holding the data
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+    """
+    """ My solution:
+    n, d = X.shape
+    K = post.shape[1]
+
+    n_hat = np.sum(post, axis=0)
+    p_hat = n_hat / n
+    mu_hat = np.asarray(
+        [np.divide((post.T @ X)[j], n_hat[j]) for j in range(K)])
+    var_hat = np.asarray([(post.T @ np.sum((X - mu_hat[j]) ** 2, axis=1))[j] /
+                          (d * n_hat[j]) for j in range(K)])
+
+    return GaussianMixture(mu=mu_hat, var=var_hat, p=p_hat)
+    """
+    # Instructor's solution: (same)
+    n, d = X.shape
+    _, K = post.shape
+
+    n_hat = post.sum(axis=0)
+    p = n_hat / n
+
+    mu = np.zeros((K, d))
+    var = np.zeros(K)
+
+    for j in range(K):
+        # Computing mean
+        mu[j, :] = (X * post[:, j, None]).sum(axis=0) / n_hat[j]
+        # Computing variance
+        sse = ((mu[j] - X) ** 2).sum(axis=1) @ post[:, j]
+        var[j] = sse / (d * n_hat[j])
+
+    return GaussianMixture(mu, var, p)
+
+def run(X: np.ndarray, mixture: GaussianMixture,
+        post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
+    """Runs the mixture model
+
+    Args:
+        X: (n, d) array holding the data
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the current assignment
+    """
+    # Instructor's solution
+    prev_ll = None
+    ll = None
+    while prev_ll is None or ll - prev_ll > 1e-6 * np.abs(ll):
+        prev_ll = ll
+        post, ll = estep(X, mixture)
+        mixture = mstep(X, post, mixture)
+
+    return mixture, post, ll
+
